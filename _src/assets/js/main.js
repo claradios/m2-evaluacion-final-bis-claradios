432925f5dd9fe2e29e648a77b3c59d5d6a08bf5a
claradios <clara.dios.diez@gmail.com>
2019-08-21 13:21:10 +0200

    add setGame function and put fetch to work

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,27 @@
 'use strict';
+const btnStart = document.querySelector('.btn__start');
+const cardsList = document.querySelector('.cards__list');
+const radioInput = document.getElementsByName('game-options');
+const backCard = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
+let cardsNumber = '';
 
-console.log('>> Ready :)');
+
+function displayCards() {
+  for (let i = 0; i < radioInput.length; i++) {
+    if (radioInput[i].checked === true) {
+      cardsNumber = radioInput[i].value;
+    }
+  }
+}
+
+
+function setGame() {
+  displayCards();
+  fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${cardsNumber}.json`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+
+
+btnStart.addEventListener('click', setGame);
